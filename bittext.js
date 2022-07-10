7838fe55cae917d4f888a626622ccb1edffcb096
russeree <reese.russell@ymail.com>
2022-07-10 03:55:10 -0700

    updates

--- bittext.js
+++ bittext.js
@@ -1,3 +1,23 @@
-require('dotenv').config();
+import http from 'http';
+import express from 'express';
+import dotenv from 'dotenv';
+import esMain from 'es-main';
+import twilio from 'twilio';
 
+dotenv.config();
 
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+class BitText{
+    constructor(){
+        console.log(`Bittext - ${process.env.BITTEXT_VERSION} started.`);
+        this.twClient = new twilio(accountSid,authToken);
+    }
+}
+
+(async()=>{
+    if(esMain(import.meta)){
+        let bitText = new BitText();
+    }
+})()
